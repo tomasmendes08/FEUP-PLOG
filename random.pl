84ad49ec428a4500fa61f3a692ca7dcd730f02e6
Pedro Coelho <up201806802@fe.up.pt>
2020-10-28 16:10:28 +0000

    Added random for spammie code bits

---
+++ random.pl
@@ -0,0 +1,27 @@
+%simbolos para stacks (caso se use isto)
+symbol(black, 1, S):- S='B1'.
+symbol(black, 2, S):- S='B2'.
+symbol(black, 3, S):- S='B3'.
+symbol(black, 4, S):- S='B4'.
+symbol(black, 5, S):- S='B5'.
+symbol(black, 6, S):- S='B6'.
+symbol(black, 7, S):- S='B7'.
+symbol(black, 8, S):- S='B8'.
+symbol(black, 9, S):- S='B9'.
+symbol(black, 10, S):- S='B10'.
+symbol(black, 11, S):- S='B11'.
+symbol(black, 12, S):- S='B12'.
+
+symbol(white, 1, S):- S='W1'.
+symbol(white, 2, S):- S='W2'.
+symbol(white, 3, S):- S='W3'.
+symbol(white, 4, S):- S='W4'.
+symbol(white, 5, S):- S='W5'.
+symbol(white, 6, S):- S='W6'.
+symbol(white, 7, S):- S='W7'.
+symbol(white, 8, S):- S='W8'.
+symbol(white, 9, S):- S='W9'.
+symbol(white, 10, S):- S='W10'.
+symbol(white, 11, S):- S='W11'.
+symbol(white, 12, S):- S='W12'.
+
