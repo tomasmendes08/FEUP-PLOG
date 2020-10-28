c76787d7de407230b34baefa3d7ab6f273afb381
Pedro Coelho <up201806802@fe.up.pt>
2020-10-28 15:35:39 +0000

    Organised code and start logic

---
+++ play.pl
@@ -0,0 +1,3 @@
+play:-
+    initial(Board),
+    printBoard(Board).
--- proj.pl
+++ proj.pl
@@ -1,39 +1,6 @@
-initial(
-[[green, white, black, green, black, green],
-[black, green, green, white, green, white],
-[green, white, black, green, black, green],
-[black, white, green, green, green, white],
-[green, green, green, green, white, black],
-[white, white, black, green, black, green]]).
+:-consult('play.pl').
+:-consult('display.pl').
 
-symbol(black, S):- S='B'.
-symbol(white, S):- S='W'.
-symbol(green, S):- S='G'.
-symbol(empty, S):- S=' '.
-
-printBoard(B):-
-    nl,
-    write('   | 0 | 1 | 2 | 3 | 4 | 5 |\n'),
-    write('---|---|---|---|---|---|---|\n'),
-    printLines(B, 0).
-
-/*X representa o nmr da linha*/
-printLines([], 6).
-printLines([H|T], X):-
-    write(' '),
-    write(X),
-    write(' '),
-    write('| '),
-    printLinesAux(H),
-    nl,
-    write('---|---|---|---|---|---|---|\n'),
-    X1 is X + 1,
-    printLines(T, X1).
-
-printLinesAux([]).
-printLinesAux([H|T]):-
-    symbol(H, S),
-    write(S),
-    write(' | '),
-    printLinesAux(T).
+start:-
+    play.
 
