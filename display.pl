b98503ccdbb71cb6e21570254aecd0adddd9cb61
Pedro Coelho <up201806802@fe.up.pt>
2020-11-01 18:08:20 +0000

    Working display

--- display.pl
+++ display.pl
@@ -7,10 +7,47 @@ initialBoard([
 [[white,0], [white,0], [black,0], [green,1], [black,0], [green,1]]
 ]).
 
-symbol(black, S):- S='B0'.
-symbol(white, S):- S='W0'.
-symbol(green, S):- S='G1'.
-symbol(empty, S):- S=' '.
+midgameBoard([
+[[white,2], [empty,0], [empty,0], [empty,0], [black,0], [green,1]],
+[[black,0], [green,1], [green,1], [empty,0], [white,2], [empty,0]],
+[[green,1], [white,0], [black,0], [empty,0], [black,0], [green,1]],
+[[empty,0], [white,0], [green,1], [green,1], [green,1], [black,0]],
+[[empty,0], [green,1], [green,1], [white,2], [empty,0], [empty,0]],
+[[black,0], [white,0], [black,0], [green,1], [black,0], [green,1]]
+]).
+
+
+
+symbol(green, 1, S):- S='G1'.
+symbol(empty, 0, S):- S='  '.
+
+symbol(black, 0, S):- S='B0'.
+symbol(black, 1, S):- S='B1'.
+symbol(black, 2, S):- S='B2'.
+symbol(black, 3, S):- S='B3'.
+symbol(black, 4, S):- S='B4'.
+symbol(black, 5, S):- S='B5'.
+symbol(black, 6, S):- S='B6'.
+symbol(black, 7, S):- S='B7'.
+symbol(black, 8, S):- S='B8'.
+symbol(black, 9, S):- S='B9'.
+symbol(black, 10, S):- S='B10'.
+symbol(black, 11, S):- S='B11'.
+symbol(black, 12, S):- S='B12'.
+
+symbol(white, 0, S):- S='W0'.
+symbol(white, 1, S):- S='W1'.
+symbol(white, 2, S):- S='W2'.
+symbol(white, 3, S):- S='W3'.
+symbol(white, 4, S):- S='W4'.
+symbol(white, 5, S):- S='W5'.
+symbol(white, 6, S):- S='W6'.
+symbol(white, 7, S):- S='W7'.
+symbol(white, 8, S):- S='W8'.
+symbol(white, 9, S):- S='W9'.
+symbol(white, 10, S):- S='W10'.
+symbol(white, 11, S):- S='W11'.
+symbol(white, 12, S):- S='W12'.
 
 printBoard(B):-
     nl,
@@ -37,7 +74,7 @@ printLinesAux([H|T]):-
     write(' | '),
     printLinesAux(T).
 
-printCell([H|T]):-
-    symbol(H, S),
+printCell([H|[H2|_]]):-
+    symbol(H, H2, S),
     write(S).
     
