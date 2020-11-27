815b552613d65a67e43745768a71be1feb8735d7
Pedro Coelho <up201806802@fe.up.pt>
2020-11-27 21:34:20 +0000

    Now for real working

--- play.pl
+++ play.pl
@@ -60,49 +60,5 @@ gameLoop(GameState):-
     ).
 
 
-iterationPrint(NextGameState):-
-    iterateBoard(NextGameState, white, WhitePoints),
-    format('\n~w points  = ~w\n', [white, WhitePoints]),
-    iterateBoard(NextGameState, black, BlackPoints),
-    format('\n~w points  = ~w\n', [black, BlackPoints]).
-
-
-iterateBoard([], _, 0).
-
-iterateBoard([H|T],Player, BoardPoints):-
-    iterateLine(H, Player, LinePoints),
-    iterateBoard(T, Player, TailPoints),
-    BoardPoints is LinePoints + TailPoints.
-
-iterateLine([], _, 0).
-
-iterateLine([H|T], Player, LinePoints):-
-    iterateCell(H, Player, CellPoints),
-    iterateLine(T, Player, TailLinePoints),
-    LinePoints is CellPoints + TailLinePoints.
-
-iterateCell([], _, 0).
-
-iterateCell(Cell, Player, Points):-
-    (
-    checkPlayer(Cell, Player),
-    count(green, Cell, Points)
-    ;
-    Points is 0
-    ).
-
-
-
-count(_, [], 0).
-count(X, [X | T], N) :-
-  !, count(X, T, N1),
-  N is N1 + 1.
-count(X, [_ | T], N) :-
-  count(X, T, N).
-
-checkGameOver(Check1):-
-    Check1 == 1.
-
-
 gameOver:-
     write('GAME OVER!!!!!\n').
--- utils.pl
+++ utils.pl
@@ -52,7 +52,62 @@ replace_columnempty([C|Cs] , Y , Z , [C|Rs]) :-
   Y1 is Y-1 ,                              
   replace_columnempty( Cs , Y1 , Z , Rs ).      
 
-%COUNT POINTS
+
+iterationPrint(NextGameState):-
+    iterateBoard(NextGameState, white, WhitePoints),
+    format('\n~w points  = ~w\n', [white, WhitePoints]),
+    iterateBoard(NextGameState, black, BlackPoints),
+    format('\n~w points  = ~w\n', [black, BlackPoints]).
+
+
+iterateBoard([], _, 0).
+
+iterateBoard([H|T],Player, BoardPoints):-
+    iterateLine(H, Player, LinePoints),
+    iterateBoard(T, Player, TailPoints),
+    BoardPoints is LinePoints + TailPoints.
+
+iterateLine([], _, 0).
+
+iterateLine([H|T], Player, LinePoints):-
+    iterateCell(H, Player, CellPoints),
+    iterateLine(T, Player, TailLinePoints),
+    LinePoints is CellPoints + TailLinePoints.
+
+iterateCell([], _, 0).
+
+iterateCell(Cell, Player, Points):-
+    (
+    checkPlayer(Cell, Player),
+    count(green, Cell, Points)
+    ;
+    Points is 0
+    ).
+
+
+
+count(_, [], 0).
+count(X, [X | T], N) :-
+  !, count(X, T, N1),
+  N is N1 + 1.
+count(X, [_ | T], N) :-
+  count(X, T, N).
+
+checkGameOver(Check1):-
+    Check1 == 1.
+
+
+countGreen([], _, Counter, NewCounter):-
+    %write(Counter),nl,
+    NewCounter = Counter.
+
+countGreen([H|T], H, OldCounter, NewCounter):-
+    NewCounter2 is OldCounter+1,
+    countGreen(T, H, NewCounter2, NewCounter).
+
+countGreen([_|T], H2, Counter, NewCounter):-
+    countGreen(T, H2, Counter, NewCounter).
+
 
 validateFirstPiece(GameState, Player, Row, Col):-
   getMatrixAt(Row, Col, GameState, Content),
